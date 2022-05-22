1f8469cd56d24679e08e3539247a3d92e6cb937e
Abdullah <shabdullah685@gmail.com>
2022-05-22 22:46:10 +0530

    level 2 half completed

---
+++ Day 3/Exercise/level2.js
@@ -0,0 +1,23 @@
+// let base = prompt("Enter base");
+// let height = prompt("Enter height");
+
+// let areaOfTrinangle = 0.5 * base * height;
+// console.log(`the area of triangle is ${areaOfTrinangle}`);
+
+//Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+// let a = prompt("Enter side a");
+// let b = prompt("Enter side b");
+// let c = prompt("Enter side c");
+
+// let perimeterOfTriangle = a + b + c;
+// console.log(`the perimeter of triangle is ${perimeterOfTriangle}`);
+
+// 3.Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+let length = prompt("Enter Length:");
+let width = prompt("Enter Width:");
+
+let areaOfRectangle = length * width;
+console.log(`The area of Rectangle is ${areaOfRectangle}`);
+
+let perimeterOfRectangle = 2 * length + width;
+console.log(perimeterOfRectangle);
