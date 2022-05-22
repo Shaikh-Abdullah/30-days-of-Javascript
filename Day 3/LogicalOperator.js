60f31de914d479ee2563026bcfd80e3bb4f16680
Abdullah <shabdullah685@gmail.com>
2022-05-22 13:24:19 +0530

    Day 3 half completed

---
+++ Day 3/LogicalOperator.js
@@ -0,0 +1,26 @@
+// && ampersand operator example
+const check1 = 4 > 2 && 8 > 6;
+const check2 = 100 > 30 && 90 > 2;
+console.log(check1, check2);
+
+// || pipe or operator, example
+const check = 3 < 6 || 6 > 9;
+const check3 = 3 > 6 || 6 > 9;
+console.log(check, check3);
+
+// || pipe or operator, example
+let checking = 4 > 3;
+let checking2 = !(4 > 3);
+let lightOn = true;
+let lightOff = !lightOn;
+let isMarried = !false;
+console.log(checking, checking2, lightOn, lightOff, isMarried);
+
+//////// Increment Operator ///////////
+let count = 4;
+console.log(count++);
+console.log(count);
+
+//Decrement Operator
+console.log(count--);
+console.log(count);
---
+++ Day 3/ternaryOperators.js
@@ -0,0 +1,13 @@
+let isRaining = true;
+isRaining ? console.log("you need rain coat") : console.log("No need");
+isRaining = false;
+isRaining ? console.log("you need rain coat") : console.log("no need");
+
+let isNumber = 5;
+isNumber > 0
+  ? console.log(`${isNumber} is positive`)
+  : console.log(`${isNumber} is negative`);
+isNumber = -5;
+isNumber < 0
+  ? console.log(`${isNumber} is negative`)
+  : console.log(`${isNumber} is positive`);
