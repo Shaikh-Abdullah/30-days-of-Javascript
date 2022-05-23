d64723981c480f0249e8bda54ed60bee72939c66
Abdullah <shabdullah685@gmail.com>
2022-05-23 12:59:30 +0530

    level 3 Completed

---
+++ Day 3/Exercise/level3.js
@@ -0,0 +1,8 @@
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth();
+let day = now.getDate();
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+console.log(`${year}-0${month}-${day} ${hour}:${minute}`);
