9ff251541202739b56ca1d15850c429179968f8a
Abdullah <shabdullah685@gmail.com>
2022-05-24 12:24:28 +0530

    day 4 initial push

---
+++ Day 4/ifElse.js
@@ -0,0 +1,46 @@
+// let num = prompt("enter any number");
+// if (num > 0) {
+//   console.log(`${num}, is a positive number`);
+// } else {
+//   console.log(`${num}, is negetive number`);
+// }
+
+let num = 3;
+if (num > 0) {
+  console.log(`${num}, is positive number`);
+}
+
+// let isRaining = true;
+// if (isRaining) {
+//   console.log("don't forget to take your rain coat");
+// }
+
+//------IF ELSE-------//
+
+let numb = 3;
+if (numb > 0) {
+  console.log(`${numb}, is a positive number`);
+} else {
+  console.log(`${numb}, is a negative number`);
+}
+
+let numbe = -3;
+if (numbe > 0) {
+  console.log(`${numbe}, is a positive number`);
+} else {
+  console.log(`${numbe}, is a negative number`);
+}
+
+let isRaining = true;
+if (isRaining) {
+  console.log("you need rain coat to go outside");
+} else {
+  console.log("No need of rain coat");
+}
+
+let isRainingOutside = false;
+if (isRainingOutside) {
+  console.log("you need rain coat to go outside");
+} else {
+  console.log("No need of rain coat");
+}
