ee09c50d28771282fd4953aad10a7c48e2348a4b
Abdullah <shabdullah685@gmail.com>
2022-05-27 11:57:37 +0530

    On day 5

---
+++ Day 5/array.js
@@ -0,0 +1,57 @@
+// const fruit = ["Mongo", "Apple", "Orange", "Lemon"];
+// const webTechs = ["HTML", "CSS", "JS", "React", "Node", "Redux"];
+// console.log(fruit);
+// console.log("Number of fruits: ", fruit.length);
+// console.log(webTechs);
+// console.log("Number of web technologies: ", webTechs.length);
+
+// const arr = [
+//   "Abdullah",
+//   20,
+//   false,
+//   { country: "India", city: "Mumbai" },
+//   { skills: ["HTML", "CSS", "JS"] },
+// ];
+// console.log(arr);
+// console.log(arr.length);
+
+// //Creating an array using split
+// let js = "JavaScript";
+// const charsInJavaScript = js.split("");
+// console.log(charsInJavaScript);
+
+// let stringCompanies = "Facebook, Google, Amazon, IBM, Microsoft";
+// const companies = stringCompanies.split(",");
+// console.log(companies);
+
+//Accessing array items using index
+// const fruits = ["Mongo", "Apple", "Orange", "Lemon"];
+// let firstFruit = fruits[0];
+// console.log(firstFruit);
+
+// let thirdFruit = fruits[2];
+// console.log(thirdFruit);
+
+// let lastIndex = fruits.length - 1;
+// lastFruit = fruits[lastIndex];
+// console.log(lastFruit);
+
+// const countries = [
+//   "Albania",
+//   "Bolivia",
+//   "Canada",
+//   "Denmark",
+//   "Ethiopia",
+//   "Finland",
+//   "Germany",
+//   "Hungary",
+//   "Ireland",
+//   "Japan",
+//   "Kenya",
+// ];
+// console.log(countries);
+// console.log(countries.length);
+// console.log(countries[5]);
+
+// let lastOne = countries.length - 1;
+// console.log(countries[lastOne]);
---
+++ Day 5/modifyingArray.js
@@ -0,0 +1,5 @@
+let number = [1, 2, 3, 4, 5];
+console.log(number);
+number[0] = 10;
+number[4] = 20;
+console.log(number);
