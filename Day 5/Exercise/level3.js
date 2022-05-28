5cac4ff77201d4fd4d4eb8af64abeb5d9b1bf43b
Abdullah <shabdullah685@gmail.com>
2022-05-28 21:52:18 +0530

    level 3 half completed

---
+++ Day 5/Exercise/level3.js
@@ -0,0 +1,8 @@
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+console.log(ages.sort());
+console.log(ages.length);
+let avg = 19 + 22 + 19 + 24 + 20 + 25 + 26 + 24 + 25 + 24;
+let findAvg = avg / 10;
+console.log(avg);
+let findRange = 26 - 19;
+console.log(findRange);
