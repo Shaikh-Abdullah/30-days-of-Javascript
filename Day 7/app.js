68f01a604234ed6481b16c761fa2853b00dfa081
Abdullah <shabdullah685@gmail.com>
2022-07-12 13:08:12 +0530

    level 1 half completed

---
+++ Day 7/app.js
@@ -0,0 +1,46 @@
+// EXERCISE No.1
+
+// function fullName() {
+//     let firstName = 'Thomas'
+//     let lastName = 'Shelby'
+//     let fullName = firstName + ' ' + lastName
+//     console.count(fullName)
+// }
+// fullName()
+
+// function fullName(firstName, lastName) {
+//     return firstName + ' ' + lastName
+// }
+
+// function addNumbers(x, y) {
+//     return x + y
+// }
+
+// function areaOfRectangle(l, b) {
+//     return l * b
+// }
+
+// function perimeterofRectangle(length, width) {
+//     let perimeter = 2 * (length + width)
+//     return perimeter
+// }
+
+function volumeOfRectPrism(length, width, height) {
+    let volume = length + width + height
+    return volume
+}
+
+function areaOfaCircle(r) {
+    let area = Math.PI * r * r
+    return area
+}
+
+function circumOfCircle(r) {
+    let circum = 2 * Math.PI * r
+    return circum
+}
+
+function density(mass, volume) {
+    let density = mass / volume
+    return density
+}
